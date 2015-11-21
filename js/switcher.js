48528eefcd772f1ac903361e2968fdda4abbb97c
japjazz <jon@piguetweb.ch>
2015-11-21 20:38:51 +0100

    Version 1
    un

---
+++ js/switcher.js
@@ -0,0 +1,40 @@
+﻿// Adapted from: http://www.kelvinluck.com/assets/jquery/styleswitch/toggle.html (extensively modified)
+
+/**
+* Stylesheet toggle variation on styleswitch stylesheet switcher.
+* Built on jQuery.
+* Under an CC Attribution, Share Alike License.
+* By Kelvin Luck ( http://www.kelvinluck.com/ )
+**/
+
+(function($) {
+		// What's up?
+		var availableStylesheets = [];
+		var activeStylesheetIndex = 0;
+		
+		// Switcheroo.
+		$.stylesheetSwitch = function(styleName)
+		{
+			$('link[@rel*=style][title]').each(
+				function(i) 
+				{
+					this.disabled = true;
+					if (this.getAttribute('title') == styleName) {
+						this.disabled = false;
+						activeStylesheetIndex = i;
+					}
+				}
+			);
+		};
+		
+		// That's what's up. 
+		$.stylesheetInit = function()
+		{
+			$('link[rel*=style][title]').each(
+				function(i) 
+				{
+					availableStylesheets.push(this.getAttribute('title'));
+				}
+			);
+		};
+})(jQuery);
